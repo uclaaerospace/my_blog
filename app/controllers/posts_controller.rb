f7810260ff0b76b55605c4dbd4d8bb059b6b316c
Yoshihisa Haruyama <uclaaerospace@gmail.com>
2015-11-22 15:19:46 +0900

    NewとCreateを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,30 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def  create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "posts/#{@post.id}"
+  end
+
+   private
+
+  # params.require(key).permit(filter)
+  # key
+  #   Strong Parameters を適用したい params の key を指定する。
+  # filter
+  #   Strong Parameters で許可するカラムを指定する。
+
+
+  def post_params
+    params.require(:post).permit(:title, :category, :body)
+  end
+end
+
   def show
     @post = Post.find(params[:id])
   end
